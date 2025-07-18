8d7ae93d8c346d081777b8a837a73fc92d18a7fd
Omar Hesham <omarhesham1192019@gmail.com>
2025-07-18 15:31:51 +0300

    Ready for Hosting Ver_3

--- frontend/src/i18n.js
+++ frontend/src/i18n.js
@@ -1,7 +1,7 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 
-// Example translation resources
+// translation resources
 const resources = {
   en: {
     translation: {
